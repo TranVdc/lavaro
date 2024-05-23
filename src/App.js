805925cf05d6070235c52efd5f917745f290758b
DuocTran <tranvanduoc2394@gmail.com>
2024-05-23 21:11:25 +0700

    applied MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Features from "./components/Features";
+import Navbar from "./components/Navbar";
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+import Alert from "@mui/material/Alert";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [showCustomTheme, setShowCustomTheme] = useState(true);
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <Navbar mode={mode} toggleColorMode={toggleColorMode} />
+      <Features />
     </div>
   );
 }
